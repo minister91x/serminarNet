2e05dab9a82a83a32ba435b751db739f82d969ed
quan91 <quan91.nt@gmail.com>
2023-09-20 21:13:04 +0700

    update code

--- ConsoleAppDemo/ClassDemo2.cs
+++ ConsoleAppDemo/ClassDemo2.cs
@@ -9,4 +9,16 @@ namespace ConsoleAppDemo
         public string FirstName { get; init; }
         public string LastName { get; init; }
     }
+
+    public struct Maze
+    {
+        // Parameterless constructor with property initialization 
+        public Maze()
+        {
+            Size = 10;
+        }
+
+        // Initialization of the property at its declaration
+        public int Size { get; set; } = 10;
+    }
 }
